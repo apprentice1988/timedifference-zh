eae8377929f223618c6da0ed7af0ac990ebaaeac
apprentice1988 <zilongji@gmail.com>
2014-09-17 14:34:26 +0800

    add github url to package desc

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   summary: "output precise time differences in Chinese, eg: 1 分钟以前",
   version: "1.0.0",
+  git: 'git@github.com:apprentice1988/timedifference-zh.git'
 });
 
 Package.onUse(function(api) {
